c8b93c6d05a05e750a4ceb17ef6cbe595c8bec31
Zenon Iwanicki <zenoniwanicki@hotmail.com>
2020-11-11 14:51:18 +0000

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConsoleApp4
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var myInherit = new MyInhering() { LocalStr = "DO DUPY", mybaseInt = 11111 };
+			Console.WriteLine($"{myInherit.mybaseInt}: ---Hello World! --- {myInherit.LocalStr}");
+			Console.ReadLine();
+		}
+	}
+
+	public class MyBase
+	{
+		public int mybaseInt { get; set; }
+	}
+
+	public class MyInhering : MyBase
+	{
+		public string LocalStr { get; set; }
+	}
+}
